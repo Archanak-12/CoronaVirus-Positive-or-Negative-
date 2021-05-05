0a52b384c63e86c3109bc6484e3f087ff8c64bcc
Archana Kumari <71704741+Archanak-12@users.noreply.github.com>
2021-05-05 13:35:04 +0530

    Update Coronacaese.py

--- Coronacaese.py
+++ Coronacaese.py
@@ -1,10 +1,8 @@
 def main(str1, str2):
     m = len(str1)
     n = len(str2)
-
-    i = 0 # Index of str1
-    j = 0 # Index of str2
-
+    i = j = 0 # Index of str1 and str2
+    
 
     while i < m and j < n:
         if str1[i] == str2[j]:
